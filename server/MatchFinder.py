fc912a46244b14376bcd2e39390f16b40ffa3fd2
lbaldwin26 <146488631+lbaldwin26@users.noreply.github.com>
2024-10-27 20:46:03 -1000

    Correctly handel if player_two is disconnected but still in queue

--- server/MatchFinder.py
+++ server/MatchFinder.py
@@ -1,46 +1,55 @@
 import time
 import random
 
-from debug import logger
 
 from threading import Thread
 from model import queue, players, States
 from packets.QueueMatch import QueueMatch
 
+def make_match(player_one, player_two):
+    gets_starting_turn = bool(random.randint(0, 1))
 
+    player_one.send_packet(
+        QueueMatch(
+            player_two.UUID,
+            gets_starting_turn,
+        )
+    )
+
+    players[player_one.UUID]["state"] = States.IN_GAME
+    players[player_one.UUID]["opponent"] = player_two.UUID
+
+    player_two.send_packet(
+        QueueMatch(
+            player_one.UUID,
+            not gets_starting_turn,
+        )
+    )
+
+    players[player_two.UUID]["state"] = States.IN_GAME
+    players[player_two.UUID]["opponent"] = player_one.UUID
+
+# Completely hacky way of doing things
 def _match_finder(queue):
     while True:
         q_len = len(queue)
         if q_len >= 2:
+            disconnected_clients_exist = False
             player_one = queue.popleft()
             player_two = queue.popleft()
 
             if not player_one.isOk:
-                logger.debug("head of the queue left before finding a match")
+                disconnected_clients_exist = True
                 queue.appendleft(player_two)
-                logger.debug(queue)
-            else:
-                gets_starting_turn = bool(random.randint(0, 1))
 
-                player_one.send_packet(
-                    QueueMatch(
-                        player_two.UUID,
-                        gets_starting_turn,
-                    )
-                )
+            if not player_two.isOk:
+                disconnected_clients_exist = True
+                queue.appendleft(player_one)
+            
 
-                players[player_one.UUID]["state"] = States.IN_GAME
-                players[player_one.UUID]["opponent"] = player_two.UUID
+            if not disconnected_clients_exist:
+                make_match(player_one, player_two)
 
-                player_two.send_packet(
-                    QueueMatch(
-                        player_one.UUID,
-                        not gets_starting_turn,
-                    )
-                )
-
-                players[player_two.UUID]["state"] = States.IN_GAME
-                players[player_two.UUID]["opponent"] = player_one.UUID
         time.sleep(2)
 
 
@@ -49,5 +58,4 @@ match_finder_thread = Thread(
     args=[queue],
 )
 
-# Exit the thread when the main thread exits
 match_finder_thread.daemon = True
